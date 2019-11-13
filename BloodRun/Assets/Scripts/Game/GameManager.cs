4477f561c70fbd33d16465d114018a27901732fd
Tomdatbenik <tomvankaathoven@gmail.com>
2019-11-13 15:31:19 +0100

    Trying to sinq with server

--- BloodRun/Assets/Scripts/Game/GameManager.cs
+++ BloodRun/Assets/Scripts/Game/GameManager.cs
@@ -7,9 +7,12 @@ public class GameManager : MonoBehaviour
     private Connection connection;
 
     public GameObject Playerprefab;
+
+    public List<GameObject> players;
     // Start is called before the first frame update
     void Start()
     {
+        players = new List<GameObject>();
         connection = (Connection)FindObjectOfType(typeof(Connection));
 
         foreach(PlayerInfo player in connection.game.GetPlayers)
@@ -34,7 +37,9 @@ public class GameManager : MonoBehaviour
                 gameObject.transform.position = new Vector3(float.Parse(player.transform.location.x), float.Parse(player.transform.location.y), float.Parse(player.transform.location.z));
                 gameObject.transform.rotation = new Quaternion(float.Parse(player.transform.rotation.x), float.Parse(player.transform.rotation.y), float.Parse(player.transform.rotation.z), float.Parse(player.transform.rotation.w));
 
-                Instantiate(gameObject);
+                gameObject = Instantiate(gameObject);
+
+                players.Add(gameObject);
             }
         }
     }
@@ -42,7 +47,27 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        foreach (PlayerInfo player in connection.game.GetPlayers)
+        {
+            foreach(GameObject gameObject in players)
+            {
+                //look in properties who the player is.
+                PlayerUsername username = gameObject.GetComponent(typeof(PlayerUsername)) as PlayerUsername;
+
+                if (username.Username == player.username)
+                {
+                    Rigidbody rb = gameObject.GetComponent(typeof(Rigidbody)) as Rigidbody;
+
+                    Vector3 location = new Vector3(float.Parse(player.transform.location.x), float.Parse(player.transform.location.y), float.Parse(player.transform.location.z)).normalized;
+
+                    Debug.Log(location);
+
+                    rb.position = new Vector3(location.x,location.y,location.z);
+                    Debug.Log(rb.transform.position);
+                    rb.MoveRotation(new Quaternion(float.Parse(player.transform.rotation.x), float.Parse(player.transform.rotation.y), float.Parse(player.transform.rotation.z), float.Parse(player.transform.rotation.w)));
+                }
+            }
+        }
     }
 
 
