2efe74991aa4e08d1040c765648d52adad0eb062
Tomdatbenik <tomvankaathoven@gmail.com>
2019-11-18 12:58:25 +0100

    Update GameManager.cs

--- BloodRun/Assets/Scripts/Game/GameManager.cs
+++ BloodRun/Assets/Scripts/Game/GameManager.cs
@@ -58,9 +58,9 @@ public class GameManager : MonoBehaviour
 
                 if (username.Username == player.username && username.Username != connection.Username)
                 {
-                    float x = float.Parse(player.transform.location.x, CultureInfo.InvariantCulture);
-                    float y = float.Parse(player.transform.location.y, CultureInfo.InvariantCulture);
-                    float z = float.Parse(player.transform.location.z, CultureInfo.InvariantCulture);
+                    float x = float.Parse(player.transform.location.x, new CultureInfo("nl"));
+                    float y = float.Parse(player.transform.location.y, new CultureInfo("nl"));
+                    float z = float.Parse(player.transform.location.z, new CultureInfo("nl"));
 
                     Rigidbody rb = gameObject.GetComponent(typeof(Rigidbody)) as Rigidbody;
 
